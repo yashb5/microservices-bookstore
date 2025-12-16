b798e53691307374de7fa8026a41d05be62af55a
Tanushree <tanushreetumane@gmail.com>
2025-12-16 23:12:07 +0530

    logging

--- service-books/src/main/java/com/sergio/bookstore/service/books/services/BookService.java
+++ service-books/src/main/java/com/sergio/bookstore/service/books/services/BookService.java
@@ -5,12 +5,14 @@ import com.sergio.bookstore.service.books.exceptions.AppException;
 import com.sergio.bookstore.service.books.mappers.BookMapper;
 import com.sergio.bookstore.service.books.repositories.BookRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class BookService {
@@ -24,11 +26,10 @@ public class BookService {
         BookDto bookDto = bookRepository.findById(bookId)
                 .map(bookMapper::toBookDto)
                 .orElseThrow(() -> new AppException("No book found with ID " + bookId, HttpStatus.NOT_FOUND));
-
+        log.debug("Fetched book: {}",  bookRepository.findById(bookId));
         BigDecimal price = circuitBreakerFactory.create("getPrice").run(() -> priceService.getPrice(bookId, bookDto),
                 t -> BigDecimal.ZERO);
         bookDto.setPrice(price);
-
         return bookDto;
     }
 }
